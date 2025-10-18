84cc6a2ada3a7f7c1e2519e05db75c0f93e1b9f2
kobeylake <k0beylake@gmail.com>
2025-10-18 23:40:48 +1000

    integration off accelerometer functions in FREERTOS Project

--- src/main.cpp
+++ src/main.cpp
@@ -49,6 +49,7 @@ volatile u_int16_t adc_2;
 volatile u_int16_t accel_x[6400];
 volatile u_int16_t accel_y[6400];
 
+static bmi3_dev dev = {0};
 
 //**************************************************************************
 // LVDT struct
@@ -83,6 +84,43 @@ void myDelayMs(int ms)
 void myDelayMsUntil(TickType_t *previousWakeTime, int ms)
 {
   vTaskDelayUntil(previousWakeTime, (ms * 1000) / portTICK_PERIOD_US);  
+}
+
+// Configure accelerometer parameters
+static int8_t set_accel_config(bmi3_dev *d) {
+  int8_t rslt;
+  bmi3_sens_config cfg = {0};
+
+  cfg.type = BMI323_ACCEL;
+
+  rslt = bmi323_get_sensor_config(&cfg, 1, d);
+  bmi3_error_codes_print_result("bmi323_get_sensor_config", rslt);
+  if (rslt != BMI323_OK) return rslt;
+
+  cfg.cfg.acc.odr      = BMI3_ACC_ODR_3200HZ;
+  cfg.cfg.acc.range    = BMI3_ACC_RANGE_2G;
+  cfg.cfg.acc.bwp      = BMI3_ACC_BW_ODR_HALF;
+  cfg.cfg.acc.avg_num  = BMI3_ACC_AVG16;
+  cfg.cfg.acc.acc_mode = BMI3_ACC_MODE_NORMAL;
+
+  rslt = bmi323_set_sensor_config(&cfg, 1, d);
+  bmi3_error_codes_print_result("bmi323_set_sensor_config", rslt);
+  if (rslt != BMI323_OK) return rslt;
+
+  // Map DRDY interrupt (optional)
+  bmi3_map_int map_int = {0};
+  map_int.acc_drdy_int = BMI3_INT1;
+  rslt = bmi323_map_interrupt(map_int, d);
+  bmi3_error_codes_print_result("bmi323_map_interrupt", rslt);
+
+  // Clear any latched status
+  uint16_t tmp = 0; (void)bmi323_get_int1_status(&tmp, d);
+  return rslt;
+}
+
+static float lsb_to_g(int16_t val, float g_range, uint8_t bit_width) {
+  const float half_scale = (float)((1UL << bit_width) / 2.0f);
+  return (val * g_range) / half_scale;
 }
 
 //**************************************************************************
@@ -198,9 +236,26 @@ static void threadAccel(void *pvParameters)
   Serial.println("Thread Acc: Started");
   while(1)
   {
-    Serial.print("ignore");
-    Serial.flush();
-    myDelayMs(100);
+     static uint16_t idx = 0;
+  uint16_t int_status = 0;
+
+  if (bmi323_get_int1_status(&int_status, &dev) == BMI323_OK) {
+    if (int_status & BMI3_INT_STATUS_ACC_DRDY) {
+      bmi3_sensor_data sd = {0};
+      sd.type = BMI323_ACCEL;
+      if (bmi323_get_sensor_data(&sd, 1, &dev) == BMI323_OK) {
+        float gx = lsb_to_g(sd.sens_data.acc.x, 2.0f, dev.resolution);
+        float gy = lsb_to_g(sd.sens_data.acc.y, 2.0f, dev.resolution);
+        float gz = lsb_to_g(sd.sens_data.acc.z, 2.0f, dev.resolution);
+        Serial.print(idx); Serial.print(',');
+        Serial.print(gx, 4); Serial.print(',');
+        Serial.print(gy, 4); Serial.print(',');
+        Serial.println(gz, 4);
+        idx++;
+      }
+    }
+  }
+  myDelayMs(500);
   }
 }
 
@@ -366,6 +421,42 @@ void setup()
 
   // Start with LED off
   digitalWrite(LED_BUILTIN, LOW);
+
+  // Start I2C bus
+  Wire.begin();
+  Wire.setClock(400000);
+
+  Serial.println("Initialising BMI323 over I2C...");
+
+  // Initialise device over I2C
+  int8_t rslt = bmi3_interface_init(&dev, BMI3_I2C_INTF);
+  bmi3_error_codes_print_result("bmi3_interface_init", rslt);
+  if (rslt != BMI323_OK) return;
+
+  
+
+  // Soft reset
+  (void)bmi323_soft_reset(&dev);
+  dev.delay_us(20000, dev.intf_ptr);
+
+  // Initialise the BMI323 sensor - error somewhere in these 3 lines which make code stop
+  rslt = bmi323_init(&dev);
+  bmi3_error_codes_print_result("bmi323_init", rslt);
+  
+  // while (1){
+  //   Serial.print("bmi323_init result: ");
+  //   Serial.println(rslt);
+  //   Serial.flush();
+  // }
+  // getting a return of -2 here. a BMI3_E_COM_FAIL 
+
+  if (rslt != BMI323_OK) return;
+
+  // Configure accelerometer
+  rslt = set_accel_config(&dev);
+  if (rslt == BMI323_OK) {
+    Serial.println("idx,raw_x,raw_y,raw_z,gx,gy,gz");
+  }
 
 
   vSetErrorLed(ERROR_LED_PIN, ERROR_LED_LIGHTUP_STATE);
